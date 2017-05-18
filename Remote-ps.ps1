3bc5bb11ff17b95f5fcde3d80b4e94f0de0de592
John Copsey <Snipzwolf@users.noreply.github.com>
2017-05-18 20:26:39 +0100

    added missing commands for setting up remote powershell

--- Remote-ps.ps1
+++ Remote-ps.ps1
@@ -1,8 +1,14 @@
-$dnsName = "SOME_NAME,SOME_IP";
-$certPath = "c:\ps-cert.crt";
+$certPath = "ps-cert.crt";
+$dnsName - "some_name,another_name,192.168.0.256"
+
+#winrm quickconfig -force
+Enable-PSRemoting -SkipNetworkProfileCheck -Force
+
+#enable basic auth
+winrm set winrm/config/service/auth '@{Basic="true"}'
 
 #create and export cert
-$Cert = New-SelfSignedCertificate -CertstoreLocation Cert:\LocalMachine\My -DnsName $dnsName;
+$Cert = New-SelfSignedCertificate -CertstoreLocation Cert:\LocalMachine\My -DnsName $dnsNam;
 Export-Certificate -Cert $Cert -FilePath $certPath;
 
 #enable remote powershell
