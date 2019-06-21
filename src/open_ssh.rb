8a0327b21c03c230494caea367e921f6a3be28b6
TheTor76 <victorchong76@gmail.com>
2019-06-21 16:15:53 +0100

    Update open_ssh.rb

--- src/open_ssh.rb
+++ src/open_ssh.rb
@@ -2,12 +2,16 @@
 
 require 'net/ssh'
 
-command = 'systemctl suspend'
-
 Net::SSH.start(ENV['SSH_ENDPOINT'], ENV['SSH_USERNAME'], :password => ENV['SSH_PASSWORD']) do |ssh|
   # capture all stderr and stdout output from a remote process
   output = ssh.exec!("hostname")
   puts output
   
-  ssh.exec "systemctl suspend"
+  if ARGV[0] == 'sleep' then	
+    ssh.exec "sudo pm-suspend"
+  elsif ARGV[0] == 'hibernate'
+    ssh.exec "sudo pm-hibernate"
+  else
+    ssh.exec "systemctl suspend"
+  end
 end
