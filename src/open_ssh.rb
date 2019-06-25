401b5dfb7d51d259edcb52df82d839c1eaffc716
TheTor76 <victorchong76@gmail.com>
2019-06-25 20:27:38 +0100

    Update open_ssh.rb

--- src/open_ssh.rb
+++ src/open_ssh.rb
@@ -1,21 +1,51 @@
 #!/usr/bin/env ruby
 gem 'net-ssh', '= 5.2.0'
 require 'net/ssh'
-puts Net::SSH::Version::CURRENT
+#puts Net::SSH::Version::CURRENT
 
 Net::SSH.start(ENV['SSH_ENDPOINT'], ENV['SSH_USERNAME'], :password => ENV['SSH_PASSWORD']) do |ssh|
   # capture all stderr and stdout output from a remote process
   #output = ssh.exec!("hostname")
   #puts output
   
-  if ARGV[0] == 'sleep' then	
-    output = ssh.exec "sudo pm-suspend"
-  elsif ARGV[0] == 'hibernate'
-    output = ssh.exec!("sudo pm-hibernate")
+  command = ''
+  if ARGV[0] == 'sleep' then
+    command = 'sudo pm-suspend'
+  elsif ARGV[0] == 'hibernate' then
+    command = 'sudo pm-hibernate'  
   else
-    output = ssh.exec!("systemctl suspend")
+    command = 'systemctl suspend'
   end
-  puts output
+
+  
+  #if ARGV[0] == 'sleep' then	
+    #output = ssh.exec "sudo pm-suspend"
+  #elsif ARGV[0] == 'hibernate'
+  #  output = ssh.exec!("sudo pm-hibernate")
+  #else
+  #  output = ssh.exec!("systemctl suspend")
+  #end
+  #puts output
+    
+  # open a new channel and configure a minimal set of callbacks, then run
+  # the event loop until the channel finishes (closes)
+  channel = ssh.open_channel do |ch|
+    ch.exec command do |ch, success|
+      raise "could not execute command" unless success
+
+      # "on_data" is called when the process writes something to stdout
+      ch.on_data do |c, data|
+        $stdout.print data
+      end
+
+      # "on_extended_data" is called when the process writes something to stderr
+      ch.on_extended_data do |c, type, data|
+        $stderr.print data
+      end
+
+      ch.on_close { puts "done!" }
+    end
+  end
 end
 
 
