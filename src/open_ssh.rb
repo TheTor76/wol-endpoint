d1eb8cbafb334cec3e381dba9ed92eb03a4d1eff
TheTor76 <victorchong76@gmail.com>
2019-06-25 20:44:25 +0100

    Update open_ssh.rb

--- src/open_ssh.rb
+++ src/open_ssh.rb
@@ -4,10 +4,7 @@ require 'net/ssh'
 #puts Net::SSH::Version::CURRENT
 
 Net::SSH.start(ENV['SSH_ENDPOINT'], ENV['SSH_USERNAME'], :password => ENV['SSH_PASSWORD']) do |ssh|
-  # capture all stderr and stdout output from a remote process
-  #output = ssh.exec!("hostname")
-  #puts output
-  
+    
   command = ''
   if ARGV[0] == 'sleep' then
     command = 'sudo pm-suspend'
@@ -16,25 +13,14 @@ Net::SSH.start(ENV['SSH_ENDPOINT'], ENV['SSH_USERNAME'], :password => ENV['SSH_P
   else
     command = 'systemctl suspend'
   end
-
-  
-  #if ARGV[0] == 'sleep' then	
-    #output = ssh.exec "sudo pm-suspend"
-  #elsif ARGV[0] == 'hibernate'
-  #  output = ssh.exec!("sudo pm-hibernate")
-  #else
-  #  output = ssh.exec!("systemctl suspend")
-  #end
-  #puts output
-    
+   
   # open a new channel and configure a minimal set of callbacks, then run
   # the event loop until the channel finishes (closes)
   channel = ssh.open_channel do |ch|
+    $stdout.print "executing command: " + command
     ch.exec command do |ch, success|
       raise "could not execute command" unless success
       
-      $stdout.print "executing command: " + command
-
       # "on_data" is called when the process writes something to stdout
       ch.on_data do |c, data|
         $stdout.print data
@@ -45,7 +31,7 @@ Net::SSH.start(ENV['SSH_ENDPOINT'], ENV['SSH_USERNAME'], :password => ENV['SSH_P
         $stderr.print data
       end
 
-      ch.on_close { puts "done!" }
+      #ch.on_close { puts "done!" }
     end
   end
 end
