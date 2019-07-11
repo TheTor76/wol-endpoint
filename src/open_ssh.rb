04d77be3ad8f78900c3ea64879aca0013272f809
TheTor76 <victorchong76@gmail.com>
2019-07-11 11:59:48 +0100

    Update open_ssh.rb

--- src/open_ssh.rb
+++ src/open_ssh.rb
@@ -1,40 +1,24 @@
 #!/usr/bin/env ruby
 gem 'net-ssh', '= 5.2.0'
 require 'net/ssh'
-#puts Net::SSH::Version::CURRENT
 
-Net::SSH.start(ENV['SSH_ENDPOINT'], ENV['SSH_USERNAME'], :password => ENV['SSH_PASSWORD']) do |ssh|
-    
-  command = ''
-  if ARGV[0] == 'sleep' then
+command = ''
+if ARGV[0] == 'sleep' then
     command = 'sudo pm-suspend'
-  elsif ARGV[0] == 'hibernate' then
+elsif ARGV[0] == 'hibernate' then
     command = 'sudo pm-hibernate'  
-  else
+else
     command = 'systemctl suspend'
-  end
-   
-  # open a new channel and configure a minimal set of callbacks, then run
-  # the event loop until the channel finishes (closes)
-  channel = ssh.open_channel do |ch|
-    $stdout.print "executing command: " + command
-    ch.exec command do |ch, success|
-      raise "could not execute command" unless success
-      
-      # "on_data" is called when the process writes something to stdout
-      ch.on_data do |c, data|
-        $stdout.print data
-      end
-
-      # "on_extended_data" is called when the process writes something to stderr
-      ch.on_extended_data do |c, type, data|
-        $stderr.print data
-      end
-
-      #ch.on_close { puts "done!" }
-    end
-  end
 end
 
-
-
+if ENV['USE_SSH_KEY'] == '0' then
+    Net::SSH.start(ENV['SSH_ENDPOINT'], ENV['SSH_USERNAME'], :password => ENV['SSH_PASSWORD']) do |ssh|
+        result = ssh.exec!(command)
+        puts result
+    end
+else
+    Net::SSH.start(ENV['SSH_ENDPOINT'], ENV['SSH_USERNAME'], :passphrase => ENV['SSH_PASSPHRASE']) do |ssh|
+        result = ssh.exec!(command)
+        puts result
+    end
+end 
