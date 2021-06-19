aa425024fc4af518a4a335c6d39be7dff2aeb798
TheTor76 <victorchong76@gmail.com>
2021-06-19 13:57:40 +0100

    Update server.js

--- src/server.js
+++ src/server.js
@@ -5,6 +5,8 @@ var net = require('net');
 var server = net.createServer();
 server.on('connection', handleConnection);
 
+var deviceStatus = 0;
+
 server.listen(process.env.LISTEN_PORT, process.env.LISTEN_IP, 511, function() {
   console.log('server listening to %j', server.address());
 });
@@ -25,22 +27,45 @@ function handleConnection(conn) {
     var sys = require('sys')
     var exec = require('child_process').exec;
     var cmd;
+    
+    var l = d.includes("test");
+    var m = d.includes("wake");
+    var n = d.includes("sleep");       
+    var o = d.includes("shutdown");
+    var p = d.includes("hibernate");    
+    var q = d.includes("status");
+    
+    if(l){
+      d = "test";
+    } else if(m){
+      d = "wake";
+    } else if(n){
+      d = "psshutdown/sleep";
+    } else if(o){
+      d = "shutdown";
+    } else if(p){
+      d = "psshutdown/hibernate";
+    } else if(q){
+      d = "status";
+    };
 
     switch(d){
+      case 'test':
+        document.write('test');
+      break
+      case 'status':
+        document.write(deviceStatus);
+      break
+      case 'wake':
+        cmd = '/app/send_wol.sh ' + process.env.WOL_MAC;
+        deviceStatus = 1;
+      break
       case 'shutdown':
       case 'psshutdown/sleep': 
       case 'psshutdown/hibernate': 
-      case 'test':
         cmd = 'ruby /app/open_winrm.rb ' + d;
-      break;
-      case 'sleep':
-      case 'hibernate':
-      case 'suspend':
-        cmd = 'ruby /app/open_ssh.rb ' + d;
-      break;
-      case 'wake':
-        cmd = '/app/send_wol.sh ' + process.env.WOL_MAC;
-      break
+        deviceStatus = 0;
+      break;      
       default:
         conn.write('unknown data value returing early');
         return;
